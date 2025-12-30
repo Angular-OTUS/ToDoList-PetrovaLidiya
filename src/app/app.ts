4ab45da8d6559d303d351547d7b65e0e503d0480
apatheiagit <apatheiar@mail.ru>
2025-12-30 11:30:36 +0300

    appcomponent

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ToDoListComponent } from './components/to-do-list/to-do-list';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ToDoListComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('todolist');
+}
